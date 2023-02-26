af9dd1416f26240dfde134a26a8ba599c9f3333a
ilja <illyadoba92@gmail.com>
2023-02-26 17:21:47 +0100

    change convert by ASCII

--- Battleship/task/src/battleship/Parsing.java
+++ Battleship/task/src/battleship/Parsing.java
@@ -20,8 +20,7 @@ public class Parsing {
         int[][] coordinates = new int[2][2];
         for (int i = 0; i < parts.length; i++) {
             String upperCasePart = parts[i].toUpperCase();
-            char substring1 = upperCasePart.charAt(0);
-            coordinates[i][0] = yConvertCharToInt(substring1);
+            coordinates[i][0] = upperCasePart.charAt(0) - '@';
             String substring2 = upperCasePart.substring(1);
             try {
                 coordinates[i][1] = Integer.parseInt(substring2);
@@ -30,43 +29,5 @@ public class Parsing {
             }
         }
         return coordinates;
-    }
-
-    private static int yConvertCharToInt(char y) {
-        switch (y) {
-            case 'A' -> {
-                return 1;
-            }
-            case 'B' -> {
-                return 2;
-            }
-            case 'C' -> {
-                return 3;
-            }
-            case 'D' -> {
-                return 4;
-            }
-            case 'E' -> {
-                return 5;
-            }
-            case 'F' -> {
-                return 6;
-            }
-            case 'G' -> {
-                return 7;
-            }
-            case 'H' -> {
-                return 8;
-            }
-            case 'I' -> {
-                return 9;
-            }
-            case 'J' -> {
-                return 10;
-            }
-            default -> {
-                return 0;
-            }
-        }
     }
 }
